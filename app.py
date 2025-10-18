f78ebd09de4e62eed692e33021a240119f19dec6
needalocaltech <60229190+needalocaltech@users.noreply.github.com>
2025-10-18 18:19:46 +0100

    at last

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ def clear_cart():
     return redirect(url_for('view_cart'))
 
 
-@app.route('/checkout')
+@app.route('/checkout',methods=['POST'])
 def checkout():
     if cart.is_empty():
         flash('Your cart is empty!', 'error')
