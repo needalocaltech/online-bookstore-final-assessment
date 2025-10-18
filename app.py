fc24435a964d6da2b06188cbb9edc121f33ae0fd
needalocaltech <60229190+needalocaltech@users.noreply.github.com>
2025-10-18 16:24:37 +0100

    UPD
    corrected for models!

--- app.py
+++ app.py
@@ -25,7 +25,7 @@
 ###########
 
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, session
-from models_not_quite_working import Book, Cart, User, Order, PaymentGateway, EmailService
+from models import Book, Cart, User, Order, PaymentGateway, EmailService
 import uuid
 
 ########
